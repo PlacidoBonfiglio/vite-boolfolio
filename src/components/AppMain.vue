d2201ba571f6acb5499e29f311222945a50e7b43
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-14 17:31:12 +0100

    added cards in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,7 @@ export default {
     name: "AppMain",
     data() {
         return {
-            exerciseList : [],
+            exercisesList : [],
         }
     },
     methods: {
@@ -13,7 +13,7 @@ export default {
             axios.get('http://127.0.0.1:8000/api/exercises')
             .then((response) => {
                 console.log(response.data.results);
-                this.exerciseList = response.data.results;
+                this.exercisesList = response.data.results;
             })
             .catch(function (error) {
                 console.log(error);
@@ -28,12 +28,41 @@ export default {
 
 <template>
     <main>
-        <div class="container">
 
+        <h1 class="text-center p-5">Vite Boolfolio</h1>
+
+        <div class="container mb-5">
+            <div class="row row-cols-6 gap-4 justify-content-center">
+                <div class="col text-center text-white" v-for="exercise in exercisesList" :key="exercise.id">
+                    <p class="mt-3">
+                        <span class="fw-bold">Nome Esercizio: </span>{{exercise.exercise_name}}
+                    </p>
+
+                    <p>
+                        <span class="fw-bold">Nome repo: </span>{{exercise.repo_name}}
+                    </p>
+
+                    <p>
+                        <span class="fw-bold">Esercizio completato: </span>{{exercise.exercise_completed}}
+                    </p>
+
+                    <p>
+                        <span class="fw-bold">Bonus dell'esercizio: </span>{{exercise.exercise_bonus}}
+                    </p>
+
+                    <p>
+                        <span class="fw-bold">Data: </span>{{exercise.date}}
+                    </p>
+                </div>
+            </div>
         </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
-
+    .col {
+        background-color: black;
+        border-radius: 10px;
+        box-shadow: 0px 5px 10px grey;
+    }
 </style>
