869d289253e79747f6c23755c3d0e9b30307ca1b
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-14 16:41:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppMain from './components/AppMain.vue';
+</script>
+
+<template>
+  <h1>APP</h1>
+</template>
+
+<style lang="scss">
+  @use "bootstrap/scss/bootstrap.scss" as *;
+  @use "./assets/styles/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h1>APP MAIN</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
